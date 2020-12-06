8a4149511053b6b921500836c1f5438781aa3609
Madi <fexius123@gmail.com>
2020-12-07 01:31:20 +0900

    added logo&styling to button

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-
+import logo1 from "./logo_transparent.png";
 import "./header.css";
 
 class AppHeader extends Component {
   render() {
     return (
       <header>
-        <div id="logo">JobList</div>
+        <div id="logo"></div>
         <div id="post_job">
           <button type="button" name="button">
             Post a Job
--- src/components/searcher/searcher.js
+++ src/components/searcher/searcher.js
@@ -2,8 +2,24 @@ import React, { Component } from "react";
 import "./searcher.css";
 class Searcher extends Component {
   render() {
-    return <section className="searcher_container">HELo</section>;
+    return (
+      <section className="searcher_container">
+        <MyForm />
+      </section>
+    );
   }
 }
-
+class MyForm extends Component {
+  render() {
+    return (
+      <form>
+        <select>
+          <option value="Ford">Ford</option>
+          <option value="Volvo">Volvo</option>
+          <option value="Fiat">Fiat</option>
+        </select>
+      </form>
+    );
+  }
+}
 export default Searcher;
